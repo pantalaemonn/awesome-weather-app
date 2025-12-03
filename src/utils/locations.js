4a07ceb31a655bf36662b34594243118d648fb4b
Jonah King <jonahkingcs@gmail.com>
2025-12-03 11:17:56 +0000

    Added route locations with their name, lon, lat, length, and difficulty grade

---
+++ src/utils/locations.js
@@ -0,0 +1,57 @@
+export const presetLocations = {
+    Fernilee: {
+        name: "Fernilee Reservoir Circular from Goyt Valley",
+        lat: 53.054288,
+        lon: -1.800043,
+        length: 6.0,
+        grade: "Easy"
+    },
+
+    Kinder: {
+        name: "Kinder Scout and Castleton Ridge from Hope",
+        lat: 53.34722,
+        lon: -1.7446005,
+        length: 13.5,
+        grade: "Moderate to hard"
+    },
+
+    Mam: {
+        name: "Mam Tor & Cave Dale from Castleton",
+        lat: 53.342791,
+        lon: -1.7776702,
+        length: 6.5,
+        grade: "Moderate"
+    },
+
+    Manifold: {
+        name: "Manifold Valley & Dovedale from Ilam",
+        lat: 53.054288,
+        lon: -1.800043,
+        length: 9.5,
+        grade: "Moderate"
+    },
+
+    Parkhouse: {
+        name: "Parkhouse & Chrome Hills from Longnor",
+        lat: 53.1955440,
+        lon: -1.9041640,
+        length: 6.7,
+        grade: "Easy to moderate"
+    },
+
+    Ringing: {
+        name: "Ringing Roger & Grindslow Knoll from Edale",
+        lat: 53.364428,
+        lon: -1.8151258,
+        length: 5.5,
+        grade: "Moderate"
+    },
+
+    Taxal: {
+        name: "Taxal Edge, Jenkin Chapel & Pym Chair from Errwood Reservoir",
+        lat: 53.270188,
+        lon: -1.9834667,
+        length: 8.5,
+        grade: "Moderate"
+    }
+}
